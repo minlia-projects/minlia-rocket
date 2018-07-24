d6a6a0124b0596dbcee1d082cb17c220ca950931
will <will@minlia.com>
2018-07-24 08:57:36 +0800

    Update sample

--- minlia-rocket-samples/minlia-rocket-sample-web/src/main/openapi/king/v1/repository/KingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.minlia.rocket.samples.web.openapi.king.v1.repository;
-
-
-import com.minlia.rocket.data.jpa.abstraction.AbstractRepository;
-import com.minlia.rocket.samples.web.openapi.king.v1.entity.King;
-
-public interface KingRepository extends AbstractRepository<King, Long> {
-
-}
